3c12e907ed80ebc5d2c00ebd27c80f3d9111c725
Vihaan1122 <75736369+Vihaan1122@users.noreply.github.com>
2021-02-12 17:56:09 +0530

    Add files via upload

---
+++ chain.js
@@ -0,0 +1,22 @@
+class chain 
+{
+    constructor(bodyA,bodyB)
+    {
+        var options={
+            bodyA:bodyA,
+            bodyB:bodyB,
+            stiffness:0.05,
+            length:10
+        }
+
+        this.Chain=Constraint.create(options)
+        World.add(world,this.Chain);
+
+    }
+
+    display(){
+        var pointA=this.Chain.bodyA.position
+        var pointB=this.Chain.bodyB.position
+        line (pointA.x,pointA.y,pointB.x,pointB.y);
+    }
+}
